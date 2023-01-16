fd350fbe5c5c85217de6e8a7eec6c1dbb0f4dd01
MaximilianoGomez2022 <110401807+MaximilianoGomez2022@users.noreply.github.com>
2023-01-16 11:49:33 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 
 import PeliculasRoute from './routes/peliculas.api.routes.js';
+import GenerosRoute from './routes/generos.api.routes.js';
+import UserRoute from './routes/users.api.routes.js';
 
 const app = express()
 
@@ -9,5 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/', PeliculasRoute)
+app.use('/', GenerosRoute)
+app.use('/', UserRoute)
 
 export default app
